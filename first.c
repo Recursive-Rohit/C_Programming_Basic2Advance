5842a19b19d4a75c79b6ae1efebc21f7afa0675a
Recursive-Rohit <rohitworkmail2512@gmail.com>
2026-03-15 01:23:49 +0530

    commiting first

---
+++ first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <limits.h>
+
+ int main()
+{  
+    //for(int i=1;i<10;i++)
+    //{
+    printf(" %d to %d\n",INT_MIN, INT_MAX);
+    //}
+    //some changes
+    return 0;
+}
